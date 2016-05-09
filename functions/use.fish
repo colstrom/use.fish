e84bfdfcb1c63b8662ef98ba7c3822186c8c9feb
Chris Olstrom <chris@olstrom.com>
2016-05-09 12:25:30 -0700

    Now with more reasonable paths and messages

--- functions/use.fish
+++ functions/use.fish
@@ -1,24 +1,35 @@
-function use --argument library
-    switch "$library"
+function use --argument module
+    switch "$module"
         case help --help -h ''
-            echo 'usage: use (library|path)' >&2
+            echo 'usage: use (module|path)' >&2
             false
         case '/*'
-            if test -d $library
-                if contains $library $fish_function_path
+            if test -d $module
+                if contains $module $fish_function_path
                     true
                 else
-                    set fish_function_path $library $fish_function_path
+                    test -z $QUIET
+                    and echo loading $module >&2
+                    set fish_function_path $module $fish_function_path
+                    true
                 end
             else
-                echo $library is not a directory >&2
+                test -z $QUIET
+                and echo $module is not a directory >&2
                 return 21
             end
         case '.*'
-            use (fish_realpath $library)
+            use (fish_realpath $module)
         case '*'
-            test -z $FISH_LIBRARY_PATH
-            and set FISH_LIBRARY_PATH $fish_config/functions.d
-            use (fish_realpath (string join / $FISH_LIBRARY_PATH $library))
+            test -z $FISH_USE_MODULES_PATH
+            and set FISH_USE_MODULES_PATH ~/.config/fish/use.modules
+            set module_path (string join / $FISH_USE_MODULES_PATH $module)
+            if test -d $module_path
+                use (fish_realpath $module_path)
+            else
+                test -z $QUIET
+                and echo cannot import from undefined module: $module >&2
+                return 2
+            end
     end
 end
